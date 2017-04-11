2b70cca299d52dd2c8014eed1f0cd54781f94709
jhonattan90 <jhonattan90>
2017-04-10 21:17:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/labdesenvolvimento/chamadocondominio/CondominioChamado.java
@@ -0,0 +1,13 @@
+package com.example.labdesenvolvimento.chamadocondominio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CondominioChamado extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condominio_chamado);
+    }
+}
